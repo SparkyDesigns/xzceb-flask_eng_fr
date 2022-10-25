a1402dda9a5140400f54abb80d4ac7d523fcc5d3
SrSparks <srsparks15@gmail.com>
2022-10-25 16:14:24 -0400

    Adding to tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,11 +2,13 @@ import unittest
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
+from translator import englishToFrench
+from translator import frenchToEnglish
 
 class TestfrenchToEnglish(unittest.TestCase):
     def test_frenchToEnglish(self):
-        self.assertEqual('Hello', 'Bonjour')
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
     def test_englishToFrench(self):
-        self.assertEqual('Bonjour', 'Hello')
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
 
 unittest.main()
